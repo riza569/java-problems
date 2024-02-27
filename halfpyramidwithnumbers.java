6ab1bd422fb6bf516e11efd2f73ca84ee0d16c3c
riza569 <rizamuhammed569@gmail.com>
2024-02-28 02:28:02 +0530

    more

--- halfpyramidwithnumbers.java
+++ halfpyramidwithnumbers.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
-public class rectanglestar {
+public class halfpyramidwithnumbers {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                System.out.print("*");
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
             }
             System.out.println();
-
-
-
         }
     }
 }
